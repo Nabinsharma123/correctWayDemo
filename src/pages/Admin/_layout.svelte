960648e51cfcaade93a6b2e6fb6ea377a33474f7
Nabinsharma123 <nabinsharma16.ns@gmail.com>
2023-01-24 13:42:02 +0530

    commit

---
+++ src/pages/Admin/_layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { authenticationStatus, userData } from "../store";
+    import { goto } from "@roxi/routify";
+</script>
+
+{#if $authenticationStatus == "SIGNED_IN" && $userData.defaultRole == "admin"}
+    <slot />
+{:else}
+    {$goto("/")}
+{/if}
